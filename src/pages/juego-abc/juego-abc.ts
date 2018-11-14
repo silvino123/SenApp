b86a7da5f715e24a0dd9081920bb4a016a42d771
silvino123 <36116136+silvino123@users.noreply.github.com>
2018-11-14 11:08:49 -0700

    vv

--- src/pages/juego-abc/juego-abc.ts
+++ src/pages/juego-abc/juego-abc.ts
@@ -3,7 +3,9 @@ import { IonicPage, NavController, NavParams,ToastController } from 'ionic-angul
 import { HttpClient} from '@angular/common/http';
 import { JuegoLetras } from '../../app/models/Letras';
 import {DOCUMENT}from '@angular/common';
-
+import { AlertController } from 'ionic-angular';
+import{ListPage} from '../../pages/list/list';
+//import { Observable } from 'rxjs';
 /**
  * Generated class for the JuegoAbcPage page.
  *
@@ -19,25 +21,104 @@ import {DOCUMENT}from '@angular/common';
 export class JuegoAbcPage implements OnInit {
 public  LetrasArray:JuegoLetras[]=[];
  public activarLetras:JuegoLetras;
+ public n: number = 0;
+ public vidas:number=3;
+  respuesta:any;
+ 
+  constructor(public  alertCtrl:AlertController, public navCtrl: NavController, public navParams: NavParams,public toastCtrl: ToastController,private http:HttpClient,@Inject(DOCUMENT) private document) {
   
-  constructor(public navCtrl: NavController, public navParams: NavParams,public toastCtrl: ToastController,private http:HttpClient,@Inject(DOCUMENT) private document) {
+    this.respuesta= false;
+    
+    this.activarLetras={
+    "letra":"",
+    "imagen":"",
+    "respuesta1":"",
+    "respuesta2":"",
+    "respuesta3":""
+
+   }
   }
 
   
   ngOnInit(){
     this.http.get('assets/JuegoLetras.json').subscribe((res:any)=>{
       this.LetrasArray=res.LetrasArray;
-      this.siguiente();
+
+     this.load();
+     
     });
+ 
+  }
+  load(){
+   if(this.LetrasArray.length>0){
+      
+    
+     let  selectLetra= Math.floor(Math.random()* this.LetrasArray.length);
+      
+      this.activarLetras= this.LetrasArray[selectLetra];
+      this.n  = this.n + 1;
+      this.vidas  = this.vidas;
+     
+   }
+
   }
   siguiente() {
-    if(this.LetrasArray.length>0){
-
-      let selectLetra= Math.floor(Math.random()* this.LetrasArray.length);
-      this.activarLetras= this.LetrasArray[selectLetra];
-     
-    
+    if(this.respuesta== false){
+      const alert = this.alertCtrl.create({
+        title: 'Seleccina una opcion!',
+        subTitle: '',
+        buttons: ['OK']
+      });
+      alert.present();
     }
+
+    else{
+    if(this.LetrasArray.length>0){
+      
+     let selectLetra= Math.floor(Math.random()* this.LetrasArray.length);
+        
+      this.activarLetras= this.LetrasArray[selectLetra];
+      this.n  = this.n + 1;
+      this.vidas  = this.vidas;
+      if(this.respuesta==this.activarLetras.letra ){
+        const toast = this.toastCtrl.create({
+          message: 'Respuesta Correcta'+ this.respuesta+ this.activarLetras.letra,
+          duration: 600,
+          position:'top'
+        });
+        toast.present();
+        
+      }
+      else{
+        const toast = this.toastCtrl.create({
+          message: 'Respuesta Incorrecta' + this.respuesta+ this.activarLetras.letra,
+          duration: 600,
+          position:'top'
+        });
+        toast.present();
+        this.vidas  = this.vidas-1;
+        this.respuesta= false;
+      }
+      if(this.n>10 && this.vidas!=0){
+        const alert = this.alertCtrl.create({
+          title: 'Categoria Completada!',
+          subTitle: '',
+          buttons: ['OK']
+        });
+        alert.present();
+        this.navCtrl.setRoot(ListPage);
+      }
+      if(this.vidas==0){
+        const alert = this.alertCtrl.create({
+          title: 'Se terminaron los intentos!',
+          subTitle: 'Categoria no completada',
+          buttons: ['OK']
+        });
+        alert.present();
+        this.navCtrl.setRoot(ListPage);
+      }
+    }
+  }
    
   }
 
